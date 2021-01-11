3386d7883815772558569b8ff1da9e4e0e613251
tyneymo <tyneymo@github.com>
2021-01-11 23:48:35 +0700

    this is just a commit
    yes, it is just a commit that serve no purpose.

--- main.cpp
+++ main.cpp
@@ -4,8 +4,12 @@
 
 int main(){
 	std::cout << "hello world" << std::endl;
+	//so we can programming in emacs. that's kind'a cool.
+	return 0;
 }
 
+void donothing(){
+  //this function do absolutely nothing
+  //so why are you bothering reading it?
 
-
-
+}

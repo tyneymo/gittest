d89c5ad74dc800504306e721433f826d1ffdeb82
tyneymo <tyneymo@github.com>
2021-01-12 01:12:13 +0700

    make main.cpp a little bit meaning
    same as title

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 /*this is a comment*/
 #include <iostream>
 #include <stdio.h>
+class X {
+public:
+  int i=0;
+  double j=0.0;
+
+public:
+  int getI(){ return i;}
+  double getD() {return j;}
+  ~X(){}
+};
+
 
 int main(){
 	std::cout << "hello world" << std::endl;
+	X x;
+	std::cout << x.getI() << std::endl;
+	std::cout << x.getD() << std::endl;
 	//so we can programming in emacs. that's kind'a cool.
 	return 0;
 }
